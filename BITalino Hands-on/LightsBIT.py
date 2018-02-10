8871b9e92bb6ebfdcd7f1c7dcfaf48a1fc0e9d0c
Hugo Silva <hugoslv@gmail.com>
2018-02-10 17:02:50 +0000

    Update LightsBIT.py

--- BITalino Hands-on/LightsBIT.py
+++ BITalino Hands-on/LightsBIT.py
@@ -15,6 +15,6 @@ toggle = 1-state['digitalChannels'][2]
 
 device.trigger([toggle, 0])
 
-print "LIGHTS ON" if toggle else "LIGHTS OFF"
+print ("LIGHTS ON" if toggle else "LIGHTS OFF")
 
 device.close()
